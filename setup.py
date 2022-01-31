beea469ccdd2a806b7d75dea19a2f7525b4f94d8
Ruslan <ruslan.dev.box@gmail.com>
2022-01-31 17:46:06 +0500

    Minor fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         "setuptools-rust",
         "singer-python>=5.0.12",
-        "urllib[secure]",
+        "urllib3[secure]",
         "requests",
         "tqdm",
         "selenium",
